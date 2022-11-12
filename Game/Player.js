8d30d8a186ae1387825c3152eb3e8f9f44e5d238
kevvie303 <117508458+kevvie303@users.noreply.github.com>
2022-11-12 14:16:07 +0100

    Added minecart, updated bg

--- Game/Player.js
+++ Game/Player.js
@@ -1,5 +1,5 @@
 class Player {
-    constructor(imgLeft, imgRight) {
+    constructor(imgLeft, imgRight, minecartLeft, minecartRight) {
         this.x = width / 2;
         this.y = height - 170;
         this.velocityX = 0;
@@ -12,16 +12,28 @@ class Player {
         this.isNotJumping = true;
         this.imgLeft = imgLeft;
         this.imgRight = imgRight;
+        this.minecartLeft = minecartLeft;
+        this.minecartRight = minecartRight;
         // 0 is left, 1 is right
         this.facingDirection = 0
 
     }
     update() {
         if (this.isMovingRight) {
+            if (minecart === false) {
             this.velocityX += 0.5
+            }
+            else if (minecart === true) {
+                this.velocityX += 0.75
+            }
         }
         if (this.isMovingLeft) {
+            if (minecart === false) {
             this.velocityX -= 0.5;
+            }
+            else if (minecart === true) {
+                this.velocityX -= 0.75
+            }
         }
         if (this.isJumping) {
             this.velocityY -= 30
@@ -60,25 +72,48 @@ class Player {
         this.isJumping = false;
     }
     draw() {
-        if (this.isMovingRight) {
-        image(this.imgRight, this.x, this.y)
-        this.facingDirection = 1
+        if (minecart === false) {
+            if (this.isMovingRight) {
+                image(this.imgRight, this.x, this.y)
+                this.facingDirection = 1
+            }
+            else if (this.isMovingLeft) {
+                image(this.imgLeft, this.x, this.y)
+                this.facingDirection = 0
+            }
+            else if (this.facingDirection === 1) {
+                image(this.imgRight, this.x, this.y)
+            }
+            else if (this.facingDirection === 0) {
+                image(this.imgLeft, this.x, this.y)
+            }
         }
-        else if (this.isMovingLeft) {
-            image(this.imgLeft, this.x, this.y)
-            this.facingDirection = 0
+        else if (minecart === true) {
+            if (this.isMovingRight) {
+                image(this.minecartRight, this.x, this.y)
+                this.facingDirection = 1
+            }
+            else if (this.isMovingLeft) {
+                image(this.minecartLeft, this.x, this.y)
+                this.facingDirection = 0
+            }
+            else if (this.facingDirection === 1) {
+                image(this.minecartRight, this.x, this.y)
+            }
+            else if (this.facingDirection === 0) {
+                image(this.minecartLeft, this.x, this.y)
+            }
         }
-        else if (this.facingDirection === 1) {
-            image(this.imgRight, this.x, this.y)
-        }
-        else if (this.facingDirection === 0) {
-            image(this.imgLeft, this.x, this.y)
-        }
-        if (this.y >= height - 170 && shop === false) {
+        if (this.y >= height - 170 && shop === false && minecart === false) {
             this.y = height - 170
             this.velocityY = 0;
         }
-        else if (this.y >= platform.y - 60 && abs((this.x + 30) - platform.x - 50) <= 75 && this.y <= platform.y - 40 && shop === false) {
+        if (this.y >= height - 180 && minecart === true) {
+            this.y = height - 180
+            this.velocityY = 0;
+        }
+        // This fixes the platform collision, so the player doesn't fall through it. Unless they press w
+        else if (this.y >= platform.y - 60 && abs((this.x + 30) - platform.x - 50) <= 75 && this.y <= platform.y - 40 && shop === false &! keyIsDown(83)) {
             this.y = platform.y - 60
             this.velocityY = 0;
         }
--- Game/sketch.js
+++ Game/sketch.js
@@ -3,11 +3,14 @@ let shopDoor;
 let platform;
 let shop = false;
 let forest = false;
+let mine = false;
 let town = true;
 let chopping = false;
+let mining = false;
 let choppingImg;
 let shopBg;
 let forestBg;
+let mineBg;
 let exitDoorShop;
 // 0 is completely transparent.
 let fade = false;
@@ -19,28 +22,42 @@ var woodcutLvl = 0;
 let woodcutProgress;
 let imgRight;
 let imgLeft;
+let minecartLeft;
+let minecartRight;
 let font;
 let woodcutLvlUpImg;
 let woodcutLvlUpY = 500
 let woodcutLvlUp = false
+let minecart = false;
+let minecartImg;
+let minecartX = 800;
+let minecartY = 460;
+let minecartSlideRight = false;
+let minecartSlideLeft = false;
+let minecartSlideValue = 0;
 function preload() {
     shopBg = loadImage("images/shop.gif")
     imgRight = loadImage("images/player_Right.gif")
     imgLeft = loadImage("images/player_Left.gif")
+    minecartLeft = loadImage("images/minecart_left.gif")
+    minecartRight = loadImage("images/minecart_right.gif")
     townBg = loadImage("images/mainmapke.png")
     blackCanvas = loadImage("images/black_intro_screen.png")
     forestBg = loadImage("images/forest.png")
     choppingImg = loadImage("images/chopping.gif")
     font = loadFont("fonts/Caveat-Regular.ttf")
     woodcutLvlUpImg = loadImage("images/woodcutLvlUp.png")
+    mineBg = loadImage("images/mine.png")
+    minecartImg = loadImage("images/minecart.png")
 }
 function setup() {
     createCanvas(1200, 600);
-    player = new Player(imgLeft, imgRight)
+    player = new Player(imgLeft, imgRight, minecartLeft, minecartRight)
     shopDoor = new Door(x = 510, y = height - 150, w = 50, h = 50);
     exitDoorShop = new Door(x = 90, y = width - 180, w = 140, h = 160)
     platform = new Platform();
     woodcutProgress = new Count(0, 100);
+    miningProgress = new Count(0, 100)
 }
 
 function draw() {
@@ -62,7 +79,7 @@ function draw() {
                 shopDoor.draw();
                 platform.draw();
             }
-        if (player.x <= 0) {
+        if (player.x <= 0 && town === true) {
             town = false
             forest = true
             player.x = 1199
@@ -76,7 +93,7 @@ function draw() {
         if (player.x <= 406 && player.x >= 884 && keyIsDown(87)) {
             chopping = false;
         }
-        if (player.x >= 1200) {
+        if (player.x >= 1200 && forest === true) {
             town = true;
             forest = false;
             player.x = 1;
@@ -98,6 +115,78 @@ function draw() {
             rect(player.x, player.y + 60, 60, 20, 15)
             if (sVal === 100) {
                 woodcutProgress.s = 0;
+                woodcutLvl += 1;
+                woodcutLvlUp = true
+                print(woodcutLvlUp)
+            }
+        }
+        if (woodcutLvlUp === true) {
+            image(woodcutLvlUpImg, player.x, woodcutLvlUpY)
+            woodcutLvlUpY --
+            if (woodcutLvlUpY <= 400) {
+                woodcutLvlUpY = 500
+                woodcutLvlUp = false;
+            }
+        }
+        if (player.x <= 0) {
+            forest = false;
+            mine = true;
+            player.x = 1199
+        }
+    }
+    if (forest === false && mine === true) {
+        background(mineBg);
+        if (minecart === false) {
+            image(minecartImg, minecartX, minecartY)
+        }
+        if (minecartSlideRight === true && minecartSlideValue <= 50) {
+            minecartX ++
+            minecartSlideValue ++
+            if (minecartSlideValue >= 50) {
+                minecartSlideRight = false;
+                minecartSlideValue = 0;
+            }
+        }
+        else if (minecartSlideLeft === true && minecartSlideValue <= 50) {
+            minecartX --
+            minecartSlideValue ++
+            if (minecartSlideValue >= 50) {
+                minecartSlideLeft = false;
+                minecartSlideValue = 0;
+            }
+        }
+        if (player.x >= 406 && player.x <= 884 && keyIsDown(87)) {
+            mining = true;
+        }
+        if (player.x <= 406 && player.x >= 884 && keyIsDown(87)) {
+            mining = false;
+        }
+        if (abs((player.x + 30) - minecartX) <= 30 && player.y <= 420 && player.y >= 400) {
+            minecart = true;
+        }
+        if (player.x >= 1200) {
+            forest = true;
+            mine = false;
+            minecart = false;
+            player.x = 1;
+        }
+        if (mining === true) {
+            image(choppingImg, player.x - 30, player.y - 53)
+            let middle = 550;
+            let sVal = miningProgress.s;
+            let Progress = map(sVal, 0, 100, 0, 60);
+            
+            fill(225, 255, 128);
+            textSize(32);
+            textFont(font)
+            text('Mining progress : '+ sVal + '%', width / 2 - 100, middle + 15);
+            
+            rect(player.x, player.y + 60, Progress, 20, 15)
+            stroke(128, 179, 80)
+            noFill();
+            rect(player.x, player.y + 60, 60, 20, 15)
+            if (sVal === 100) {
+                miningProgress.s = 0;
                 woodcutLvl += 1;
                 woodcutLvlUp = true
                 print(woodcutLvlUp)
@@ -154,18 +243,34 @@ function keyPressed() {
     if (keyCode === 32) {
     player.jumpUp();
     }
-    if (abs(player.x - shopDoor.x) < 25 && keyCode === 87 && shop === false && town === true) {
+    if (abs(player.x - shopDoor.x) < 25 && keyCode === 87 && shop === false && town === true && fade === false) {
         shop = true;
         fade = true;
+        player.x = 90
         fadeBg = 0;
     }
-    if (abs(player.x - exitDoorShop.x) < 110 && keyCode === 87 && shop === true) {
+    if (abs(player.x - exitDoorShop.x) < 110 && keyCode === 87 && shop === true && fade === false) {
         shop = false;
         fade = true;
+        player.x = 510
         fadeBg = 0;
     }
     if (player.x >= 406 && player.x <= 884 && forest === true && keyCode === 87 && chopping === false) {
         chopping = true
         woodcutProgress.start();
+    }
+    if (player.x >= 406 && player.x <= 884 && mine === true && keyCode === 87 && mining === false) {
+        mining = true
+        miningProgress.start();
+    }
+    if (keyCode === 32 && minecart === true && player.facingDirection === 1) {
+        minecart = false;
+        minecartX = player.x
+        minecartSlideRight = true;
+    }
+    if (keyCode === 32 && minecart === true && player.facingDirection === 0) {
+        minecart = false;
+        minecartX = player.x
+        minecartSlideLeft = true;
     }
 }
